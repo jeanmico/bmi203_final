8f8703d1e504f386214ff0190979ce918b9eae2e
jeanmico <costellojeanm@gmail.com>
2018-03-21 21:06:26 -0700

    updated roc

--- final_project/neuralnet_rd.py
+++ final_project/neuralnet_rd.py
@@ -26,9 +26,16 @@ def training_data():
 
     with open(os.path.join(file_path, classname)) as f:
         for line in f:
-            classes.append(line.strip())
+            lineval = line.strip().split('\t')
+            classes.append(lineval)
 
-    return (string_to_array(sequences), np.asarray(classes, dtype=int))
+    class_array = np.zeros((len(classes), 2))
+
+    for i, class_member in enumerate(classes):
+        class_array[i][0] = class_member[0]
+        class_array[i][1] = class_member[1]
+
+    return (string_to_array(sequences), class_array)
 
 
 def string_to_array(binary_strings):
@@ -45,25 +52,21 @@ def string_to_array(binary_strings):
 
 
 ## GLOBAL PARAMETERS
-
-#seqs, y = training_data()
-
-
+seqs, y = training_data()
 
 # model size
-#input_dim = 68 # (length, in binary, of strings: number of columns in seqs)
-#output_dim = 2
-#example_ct = len(seqs) 
+input_dim = 68 # (length, in binary, of strings: number of columns in seqs)
+output_dim = 2
 
-seqs = np.identity(8)
-y = np.identity(8)
-print(y.shape)
-print(type(y))
+#seqs = np.identity(8)
+#y = np.identity(8)
+#print(y.shape)
+#print(type(y))
 
 example_ct = len(seqs) 
 
-input_dim = 8
-output_dim = 8
+#input_dim = 8
+#output_dim = 8
 
 # gradient descent params
 epsilon = 0.01
@@ -71,7 +74,9 @@ lambda_reg = 0.01
 
 
 def activation(x):
-
+    """
+    sigmoid function
+    """
     return expit(x)
 
 
@@ -112,24 +117,21 @@ def class_predict(model, seq):
     classifies input as true binding site or not (1 or 0)
     """
 
-    w1, b1, w2, b2 = model['w1'], model['b1'], model['w2'], model['b2']
 
-    #forward propagation
-
-    z1 = seq.dot(w1) + b1
-    a1 = expit(z1)  # sigmoid function
-    z2 = a1.dot(w2) + b2
-    exp_scores= np.exp(z2)
-    probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
-    #return np.argmax(probs, axis=1)
-    #return np.argmax(probs)
-    if np.argmax(probs)==0:
-        return 1 - np.max(probs)
-    else:
-        return np.max(probs)
 
 def roc_score(true_scores, model_output):
-    roc = metrics.roc_auc_score(true_scores, model_output)
+    true_scores_flat = []
+    for score in true_scores[:,]:
+        true_scores_flat.append(np.argmax(score))
+
+    model_output_flat = []
+    for output in model_output[:,]:
+        if np.argmax(output) == 0:
+            model_output_flat.append(1 - output[0])
+        else:
+            model_output_flat.append(output[1])
+
+    roc = metrics.roc_auc_score(true_scores_flat, model_output_flat)
     return roc
 
 def backprop(net, seq, y):
@@ -147,15 +149,11 @@ def model_build(nodes_in_hidden, passes):
     """
 
     np.random.seed(100)
-    input_dim =8
-    output_dim =8
+    #input_dim = 68
+    #output_dim =8
 
-    w1 = 2*np.random.randn(input_dim, nodes_in_hidden) - 1
-    w2 = 2*np.random.randn(nodes_in_hidden, output_dim) - 1
-
-    print(w1.shape)
-    print(w2.shape)
-
+    w1 = 2*np.random.randn(input_dim, nodes_in_hidden) - 1 # from layer0 to hidden
+    w2 = 2*np.random.randn(nodes_in_hidden, output_dim) - 1 # from hidden to output
     model = dict()  # dictionary to hold the parameters
 
     for i in range(passes):
@@ -167,13 +165,10 @@ def model_build(nodes_in_hidden, passes):
         layer1 = activation(np.dot(layer0, w1))
         layer2 = activation(np.dot(layer1, w2))
 
-        print('l1: ' + str(layer1.shape))
-        print(layer2.shape)
 
         l2_error = y - layer2
 
         l2_delta = l2_error*activation(layer2)
-        print('l2delt ' + str(l2_delta.shape))
 
         l1_error = l2_delta.dot(w2.T)  # this is the backpropagation step!!
 
@@ -181,26 +176,26 @@ def model_build(nodes_in_hidden, passes):
 
         # update!
         w2 += layer1.T.dot(l2_delta)
-        w1 += layer0.dot(l1_delta)
+        w1 += layer0.T.dot(l1_delta)
 
 
-    print(layer2)
+    return layer2
 
 
 def neural_net():
     # build and train the model
-    hidden_nodes = 3
-    passes = 120000
-    model = model_build(hidden_nodes, passes)
+    hidden_nodes = 10
+    passes = 20000
+    model_output = model_build(hidden_nodes, passes)
 
-    model_output = []
-    for i in seqs:
-        model_output.append(class_predict(model, i))
+
+    #for i in seqs:
+    #    model_output.append(class_predict(model, i))
     
     roc = roc_score(y, np.asarray(model_output, dtype=float))
     print(roc)
 
-    with open(os.path.join(file_path, "tracking.txt"), 'a') as output:
+    with open(os.path.join(file_path, "tracking_rd.txt"), 'a') as output:
         output.write('\n' + str(hidden_nodes) + ' ' + str(passes) + ' ' + str(roc))
 
 
