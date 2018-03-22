5f3575b16a3f1b6426963bbab3b0781a0894feec
jeanmico <costellojeanm@gmail.com>
2018-03-22 15:34:39 -0700

    added string to array function

--- final_project/utils.py
+++ final_project/utils.py
@@ -1,10 +1,10 @@
-
+import numpy as np
 
 
 def to_binary(seqs):
-	"""
+    """
     takes in list of sequences, returns list of sequences in binary
-	"""
+    """
     bin_dict = dict()
     bin_dict["A"] = "1000"
     bin_dict["C"] = "0100"
@@ -13,9 +13,25 @@ def to_binary(seqs):
 
     bin_seqs = []
     for seq in seqs:
-    	bin_seq = ""
+        bin_seq = ""
         for char in seq:
             bin_seq += bin_dict[char]
         bin_seqs.append(bin_seq)
 
     return bin_seqs
+
+
+def string_to_array(binary_strings):
+    """
+    takes in list of binary strings, outputs numpy array
+    """
+    rows = len(binary_strings)
+    cols = len(binary_strings[0])
+
+    seq_array = np.zeros((rows, cols), dtype=int)
+
+    for i, seq in enumerate(binary_strings):
+        for j, char in enumerate(seq):
+            seq_array[i, j] = int(char)
+
+    return seq_array
