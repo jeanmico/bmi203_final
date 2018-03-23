65d4f8bbf7c24ceacea8a2dc9a8d167dfd9cc971
jeanmico <costellojeanm@gmail.com>
2018-03-23 16:08:50 -0700

    string to array test

--- test/test_nn.py
+++ test/test_nn.py
@@ -8,6 +8,15 @@ def test_convert_to_binary():
     bin_seq = utils.to_binary(seq)
     assert bin_seq == ["1000010000100001"]
 
+
 def test_string_to_array():
     # verify that we can convert a list of strings to arrays
-    assert 1 == 1
+    binary_sequences = ["1000010000100001", "0100010000100001"]
+
+    binary_array = utils.string_to_array(binary_sequences)
+
+    verification_array = np.array([[1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+       [0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1]])
+
+    assert np.array_equal(binary_array, verification_array)
+
