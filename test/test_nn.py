0c8eab75dd604b6de56267375f6f329990675699
jeanmico <costellojeanm@gmail.com>
2018-03-22 19:49:02 -0700

    updated module structure

--- test/test_nn.py
+++ test/test_nn.py
@@ -1,9 +1,13 @@
 import pytest
+import numpy as np
 from final_project import utils
 
 def test_convert_to_binary():
     # check that nucleotides are correctly written
-    seq = "ACGT"
+    seq = ["ACGT"]
     bin_seq = utils.to_binary(seq)
+    assert bin_seq == ["1000010000100001"]
 
-    assert bin_seq == "1000010000100001"
+def test_string_to_array():
+    # verify that we can convert a list of strings to arrays
+    assert 1 == 1
