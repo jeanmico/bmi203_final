71dc74d0e9d91b3741ebbecb8e9d1ec71cc09739
jeanmico <costellojeanm@gmail.com>
2018-03-23 00:47:50 -0700

    finalized cross-validation

--- final_project/neuralnet_rd.py
+++ final_project/neuralnet_rd.py
@@ -1,13 +1,7 @@
 # implements a neural network
 
-#TODO:
-# gradient descent with backpropagation
-# better training scheme
-# cross-validation
-# test scenarios
-# fix derivatives
-# WRITE-UP
-# parameter testing: hidden neurons, epsilon, (optional reg_lambda)
+#TODO
+# really gorgeous comments!
 
 import sys
 import os
@@ -15,10 +9,11 @@ import numpy as np
 from final_project import utils
 from scipy.special import expit
 import sklearn.metrics as metrics
+import random
+import math
 
 
 file_path = os.path.join(os.path.sep, 'Users', 'student', 'GitHub', 'bmi203_final')
-teststr = ['10000100100000010100010000100001001001001000010001000001010001000010']
 
 ## GLOBAL PARAMETERS
 
@@ -28,14 +23,63 @@ hidden_nodes = 10
 passes = 1000
 
 # gradient descent params
-epsilon = 0.01  # learning rate for gradient descent
+epsilon = .1  # learning rate for gradient descent
+lambda_reg = .1  #regularization term
 
 
-def crossval():
+def crossval_sets(pos_ind, neg_ind, seqs, classes):
+    testsize = .75
+
+    random.shuffle(pos_ind)
+    random.shuffle(neg_ind)
+
+    poslen = math.floor(testsize * len(pos_ind))
+    neglen = math.floor(testsize * len(neg_ind))
+
+    pos_train = pos_ind[:poslen]
+    neg_train = neg_ind[:neglen]
+
+    train_ind = pos_train + neg_train
+
+    train_seq = []
+    train_class = []
+    test_seq = []
+    test_class = []
+
+    for i in range(len(seqs)):
+        if i in train_ind:
+            train_seq.append(seqs[i])
+            train_class.append(classes[i])
+        else:
+            test_seq.append(seqs[i])
+            test_class.append(classes[i])
+
+
+    return np.asarray(train_seq), np.asarray(train_class), np.asarray(test_seq), np.asarray(test_class)
+
+
+def crossval(seqs, y):
     """
     takes in a dataset
     returns two datasets: training and testing
     """
+
+    # there has to be a better way to do this, but np.where is confusing
+    full_index = set([x for x in range(len(y))])
+    pos_index = set()
+
+    for index, i in enumerate(y):
+        if i[0] == 0:
+            pos_index.add(index)
+
+    print(len(pos_index))
+    neg_index = full_index - pos_index
+
+    full_index = list(full_index)
+    neg_index = list(neg_index)
+    pos_index = list(pos_index)
+
+    return pos_index, neg_index
 
 
 def activation(x, deriv=False):
@@ -49,33 +93,28 @@ def activation(x, deriv=False):
         return sigmoid_out
 
 
-def activation_to_deriv():
-    return 
-
-
-def loss_compute(model):
-    """
-    determines performance of model
-    """
-
-
-
-def class_predict(model, seq):
+def class_predict(model, test_seqs, roc=False):
     """
     classifies input as true binding site or not (1 or 0)
     """
     w1 = model['w1']
     w2 = model['w2']
     # forward propagation
-    l1 = activation(np.dot(seq, w1))
+    l1 = activation(np.dot(test_seqs, w1))
     l2 = activation(np.dot(l1, w2))
 
+    if roc:
+        return l2
+
+    predictions = []
     # return output as a single number
-    max_arg = np.argmax(l2)
-    if max_arg == 0:
-        return (1 - l2[max_arg])
-    else:
-        return l2[max_arg]
+    for output in l2[:,]:
+        max_arg = np.argmax(l2)
+        if max_arg == 0:
+            predictions.append(1 - l2[max_arg])
+        else:
+            predictions.append(l2[max_arg])
+    return predictions
 
 
 def roc_score(true_scores, model_output):
@@ -92,12 +131,6 @@ def roc_score(true_scores, model_output):
 
     roc = metrics.roc_auc_score(true_scores_flat, model_output_flat)
     return roc
-
-def backprop(net, seq, y):
-    """
-
-    """
-    print('ok')
 
 
 def model_build(seqs, y, nodes_in_hidden, passes):
@@ -131,12 +164,12 @@ def model_build(seqs, y, nodes_in_hidden, passes):
         l1_delta = l1_error*activation(layer1, True)
 
         # update our weights
-        w2 += layer1.T.dot(l2_delta)
-        w1 += layer0.T.dot(l1_delta)
+        w2 += epsilon * (layer1.T.dot(l2_delta) + lambda_reg*w2)
+        w1 += epsilon * (layer0.T.dot(l1_delta) + lambda_reg*w1)
 
-        if i%1000 == 0:
+        if i%100 == 0:
             error = np.mean(np.abs(l2_error))
-            print(error)
+            #print(error)
             errors.append(error)
 
     model['w1'] = w1
@@ -160,14 +193,22 @@ def neural_net():
 
 
     else:
+
         #subset your data for cross-validation
-        for j in range(20):
-            seqs, y = crossval()
+        master_seqs, master_class = utils.training_data(file_path, "training.txt", output_dim)
+        pos_ind, neg_ind = crossval(master_seqs, master_class)
+
+        master_seqs.tolist()
+        master_class.tolist()
+
+        for j in range(25):
+            seqs, y, seq_test, y_test = crossval_sets(pos_ind, neg_ind, master_seqs, master_class)
             model, model_output = model_build(seqs, y, hidden_nodes, passes)
-        
-            roc = roc_score(y, np.asarray(model_output, dtype=float))
+            
+            predictions = class_predict(model, seq_test, True)
+            roc = roc_score(y_test, np.asarray(predictions, dtype=float))
             print(roc)
 
-            with open(os.path.join(file_path, "tracking_cv.txt"), 'a') as output:
-                output.write('\n' + str(hidden_nodes) + ' ' + str(passes) + ' ' + str(roc))
+            with open(os.path.join(file_path, "crossval_roc.txt"), 'a') as output:
+                output.write('\n' + str(j) + ' ' + str(roc))
 
